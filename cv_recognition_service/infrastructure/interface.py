470b4d393dad53b7e6435c3bafdfba57cedcb31c
Artur <iar@napoleonit.ru>
2023-06-24 14:55:15 +0500

    this commit is full of shit

---
+++ cv_recognition_service/infrastructure/interface.py
@@ -0,0 +1,78 @@
+from abc import abstractmethod, ABC
+from dataclasses import dataclass
+from typing import List
+
+import numpy as np
+
+
+@dataclass
+class Action:
+    class_name: str
+    class_id: str
+    score: float
+
+
+@dataclass
+class ClassificationData:
+    emotions: List[Action]
+
+
+class ClassificationModel(ABC):
+    @abstractmethod
+    def _load_model(self, model_path: str):
+        """
+        Load model from local file
+        """
+        pass
+
+    @abstractmethod
+    def _image_preprocessing(self, image: np.ndarray):
+        """
+        Preprocess input images
+        """
+
+    @abstractmethod
+    def predict(self, image: np.ndarray) -> ClassificationData:
+        """
+        Predict action of detected vehicle
+        """
+        pass
+
+
+@dataclass
+class DetectionData:
+    x_min: int
+    y_min: int
+    x_max: int
+    y_max: int
+    score: float
+    class_name: str = "truck"
+    tracking_id: int = None
+    activity: int = 0
+
+    def __post_init__(self):
+        self.width = self.x_max - self.x_min
+        self.height = self.y_max - self.y_min
+        self.bbox = (self.x_min, self.y_min, self.width, self.height)
+
+
+class DetectionModel:
+    @abstractmethod
+    def _load_model(self, model_path: str):
+        """
+        Load model from local file
+        """
+        pass
+
+    @abstractmethod
+    def _image_preprocessing(self, image: np.ndarray):
+        """
+        Preprocess input images
+        """
+
+    @abstractmethod
+    def detect(self, image: np.ndarray) -> List[DetectionData]:
+        """
+        Get detections from the image
+        """
+        pass
---
+++ cv_recognition_service/service/recognition.py
@@ -0,0 +1,31 @@
+from typing import List
+import numpy as np
+
+from infrastructure.interface import DetectionModel
+from infrastructure.interface import ClassificationModel
+from infrastructure.tracking.wrapper import DeepsortTracker
+from service.interface import BaseService, FrameData
+
+
+class DummyRecognitionService(BaseService):
+    def __init__(
+        self,
+        detection_model: DetectionModel,
+        classification_model: ClassificationModel,
+        det_threshold=0.9,
+        cls_thresh=0.9,
+    ):
+
+        self.detection_model = detection_model
+        self.classification_model = classification_model
+
+    def process_frame(self, frame: np.ndarray) -> FrameData:
+        detections_data = self.detection_model.detect(frame)
+
+        cropped_images = self._crop_images(frame, detections_data)
+        classes_data = [
+            self.classification_model.predict(img) for img in cropped_images
+        ]
+        frame_data = self._serialize_frame_data(detections_data, classes_data)
+        plotted_frame = self._draw_predictions(frame, frame_data)
+        return plotted_frame
