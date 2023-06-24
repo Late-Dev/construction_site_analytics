4ecb5a72f95a0d6d7cc2f79323ecb42d13545aea
traptrip <aletni@gmail.com>
2023-06-25 04:39:11 +0500

    add activity detection logic

--- cv_recognition_service/building.py
+++ cv_recognition_service/building.py
@@ -1,7 +1,11 @@
 def build_recognition_service():
     from infrastructure.detection import YOLODetector
+    from infrastructure.activity_detection import ActivityDetector
     from service.recognition import RecognitionService
 
-    detector = YOLODetector(model_path="models/detector.pt", conf_thresh=0.6, device="cuda:0")
-    service = RecognitionService(detector)
+    detector = YOLODetector(
+        model_path="models/detector.pt", conf_thresh=0.6, device="cuda:0"
+    )
+    activity_detector = ActivityDetector()
+    service = RecognitionService(detector, activity_detector)
     return service
