71fa8386e24b6905106da33e235c3206a6ead41f
traptrip <aletni@gmail.com>
2023-06-24 15:22:09 +0500

    add tracking

--- detector_training/detect.py
+++ detector_training/detect.py
@@ -2,8 +2,8 @@ from pathlib import Path
 from ultralytics import YOLO
 
 # Load a model
-model = YOLO('runs/detect/train_yolov8m/weights/best.pt')
+model = YOLO(Path(__file__).parent / 'runs/detect/train_yolov8m/weights/best.pt')
 
 # Train the model with 2 GPUs
-data_path = (Path(__file__).parent / './datasets/construction_analytics.yaml').resolve()
-model.predict("datasets/construction_analytics/videos/output012.mp4", save=True, imgsz=640, conf=0.5, device=[0])
+data_path = (Path(__file__).parent / 'datasets/construction_analytics.yaml').resolve()
+model.track(Path(__file__).parent / "datasets/construction_analytics/videos/output012.mp4", save=True, imgsz=640, conf=0.5, device=[0])
